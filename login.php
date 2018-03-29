fc5c43e3ec88b4e8ee8b7c0d6904311ba7dc757a
Brooke <Brookealynn@Brookes-MacBook-Pro.local>
2018-03-29 18:01:16 -0400

    working

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE>
+<!DOCTYPE><!DOCTYPE>
 <html>
 
 	<head>
@@ -12,10 +12,10 @@
 		<nav>
 			<!-- nav bar stuff -->
 		<ul>
-		  <li><a href="./index.html">Home</a></li>
+		  <!-- <li><a href="./index.html">Home</a></li>
 		  <li><a href="./purchasetickets.html">Purchase Tickets</a></li>
 		  <li><a href="./browsemovies.php">Browse Movies</a></li>
-		  <li><a href="#about">Theatres</a></li>
+		  <li><a href="#about">Theatres</a></li> -->
 		  <li style="float:right"><a class="active" href="./login.php">Login</a></li>
 		  <li style="float:right"><a class="active" href="./signup.php">Sign Up</a></li>
 		</ul>
@@ -25,11 +25,12 @@
 
 		<!-- form test -->
 		<form action="login.php" method="post">
-
+		<center>
 		<p>Account Number:</p>
 		<input type="text" id="textbox" name="firstname">
 		<input name="login" type="submit">
 		</form>
+		</center>
 		
 		<?php
 			session_start();
@@ -37,32 +38,40 @@
 
 			if($mysqli == false) {
 				die("Error" . mysqli_connect_error());
-					if((isset($_POST['login']))) {
-		}
+			}
 			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				// if ($_POST['login'] == $_POST['confirm-login']){
+				if((isset($_POST['login']))) {
 					//value of input
 					$account_num = $_POST['firstname'];
-				}
-
-				$sql = "SELECT * FROM `customers` WHERE ACCOUNT_NUM= '$account_num'";
+					// echo $account_num;
+				} 
+				$sql = "SELECT * FROM customers WHERE ACCOUNT_NUM= '$account_num'";
 				//Query
 				$result = mysqli_query($mysqli, $sql) or die();	
-
 				//Check if the value exists in the table Customers
 				//Getting value at account_num
-				while ($row = mysqli_fetch_array($result)) {
-					$myString .= $row['ACCOUNT_NUM'] . " ";
-				}
-
+				// while ($row = mysqli_fetch_array($result)) {
+				// 	$myString .= $row['ACCOUNT_NUM'] . " ";
+				// }
 				$count = mysqli_num_rows($result);
 				//if query returns 1 row, set a session for that email, redirect to newpage.php
+				if ($count !==1) {
+					echo "login failed";
+				}
+
+				$sql2 = "SELECT * FROM customers WHERE ACCOUNT_NUM= '$account_num'";
+
+
+				$result2 = mysqli_query($mysqli, $sql2);
+
+				if ($result == NULL) {
+					header("location: theatrecomplex.php");
+				} 
 				if ($count == 1) {
 					header("location: newpage.php");
-				} else {
-					echo "login failed";	
 				} 
-			}
+			 }
 		
 		?>
 

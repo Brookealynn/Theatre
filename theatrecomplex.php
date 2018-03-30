5859df73dc7f1d8c8faa4bd486e3788dfd954ada
Brooke <Brookealynn@Brookes-MacBook-Pro.local>
2018-03-29 21:18:03 -0400

    working

--- theatrecomplex.php
+++ theatrecomplex.php
@@ -12,11 +12,10 @@
 		<nav>
 			<!-- nav bar stuff -->
 		<ul>
-		  <li><a href="./index.html">Home</a></li>
+		  <!-- <li><a href="./index.html">Home</a></li>
 		  <li><a href="./purchasetickets.html">Purchase Tickets</a></li>
 		  <li><a href="./browsemovies.php">Browse Movies</a></li>
-		  <li><a href="./theatrecomplex.php">Choose Theatre Complex</a></li>
-		  <li style="float:right"><a class="active" href="./login.p</a></li>
+		  <li><a href="#about">Theatres</a></li> -->
 		  <li style="float:right"><a class="active" href="./login.php">Login</a></li>
 		  <li style="float:right"><a class="active" href="./signup.php">Sign Up</a></li>
 		</ul>
@@ -25,12 +24,15 @@
 	</header>
 
 		<!-- form test -->
-		<form action="theatrecomplex.php" method="post">
-
-		<p>Choose A Theatre Complex:</p>
-		<input type="text" id="name" name="theatrecomplexname">
-		<input name="theatrecomplex" type="submit">
+		<form action="login.php" method="post">
+		<center>
+		<p>Account Number:</p>
+		<input type="text" id="textbox" name="accountnum">
+		<p>Password:</p>
+		<input type="text" id="textbox" name="password"> <br>
+		<input name="login" type="submit">
 		</form>
+		</center>
 		
 		<?php
 			session_start();
@@ -41,31 +43,41 @@
 			}
 			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				// if ($_POST['login'] == $_POST['confirm-login']){
-				if((isset($_POST['theatrecomplex']))) {
+				if(isset($_POST['accountnum']) && isset($_POST['password'])) {
 					//value of input
-					$complex_name = $_POST['theatrecomplexname'];
-					echo "$complex_name";
-				}
-
-				$sql = "SELECT * FROM `theater_complexes` WHERE COMPLEX_NAME= '$complex_name'";
+					$account_num = $_POST['accountnum'];
+					$account_password = $_POST['password'];
+					// echo $account_num;
+				} 
+				$sql = "SELECT * FROM `customers` WHERE ACCOUNT_NUM= '$account_num' AND ACCOUNT_PASSWORD = '$account_password'";
 				//Query
 				$result = mysqli_query($mysqli, $sql) or die();	
 				echo "hi";
-
 				//Check if the value exists in the table Customers
 				//Getting value at account_num
-				while ($row = mysqli_fetch_array($result)) {
-					$myString .= $row['COMPLEX_NAME'] . " ";
-				}
-
+				// while ($row = mysqli_fetch_array($result)) {
+				// 	$myString .= $row['ACCOUNT_NUM'] . " ";
+				// }
 				$count = mysqli_num_rows($result);
 				//if query returns 1 row, set a session for that email, redirect to newpage.php
-				if ($count == 1) {
-					header("location: newpage.php");
-				} else {
-					echo "This Theatre Complex Does Not Exist";	
+				if ($count !==1) {
+					echo "login failed";
+				}
+				else{
+					$sql2 = "SELECT * FROM `customers` WHERE ACCOUNT_NUM= '$account_num' AND ACCOUNT_PASSWORD = '$account_password'";
+
+
+					$result2 = mysqli_query($mysqli, $sql2);
+
+					$row = $result2 -> fetch_assoc();
+					if (($row["PRIMARY_COMPLEX"]) == NULL) {
+						header("location: selectprimarycomplex.php");
 				} 
-			}
+				}
+
+
+				
+			 }
 		
 		?>
 
